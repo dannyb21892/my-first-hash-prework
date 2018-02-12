ecb47b8f61b63993b4acebbb5a5936707092d69a
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-12 20:04:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,15 +1,14 @@
 def my_hash
   your_hash = Hash.new
-  your_hash["dannyb" => "OoT player"]
+  your_hash["dannyb"] = "OoT player"
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = {}
+  the_manifest["whale bone corsets"] = 5
+  the_manifest["porcelain vases"] = 2
+  the_manifest["oil paintings"] = 3
 end
 
 def retrieval
